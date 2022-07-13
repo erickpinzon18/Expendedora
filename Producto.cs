da5c8b46d161a506527908139ddcfc68bad7844d
Erick Pinzon <erickpinzonhuerta@gmail.com>
2022-07-13 12:10:22 -0500

    Cambios

--- Producto.cs
+++ Producto.cs
@@ -1,23 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Expendedora
+﻿namespace Expendedora
 {
     internal class Producto
     {
-        private int Clave;
-        private string Nombre;
-        private double Precio;
-        private int Stock;
-        private double totVenta;
-
-        public int Clave1 { get => Clave; set => Clave = value; }
-        public string Nombre1 { get => Nombre; set => Nombre = value; }
-        public double Precio1 { get => Precio; set => Precio = value; }
-        public int Stock1 { get => Stock; set => Stock = value; }
-        public double TotVenta { get => totVenta; set => totVenta = value; }
     }
 }
